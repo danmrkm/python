61d054d268339344487c59f059d59f7e00caf84c
danmrkm <38092962+danmrkm@users.noreply.github.com>
2018-06-25 03:12:20 +0900

    In house

--- scripts/parse_json.py
+++ scripts/parse_json.py
@@ -5,6 +5,32 @@ import sys
 import json
 
 
+def search_key(args, position, key_list):
+
+    # print("args: %s, position: %s, key_list: %s \n" % (type(args), type(position), type(key_list)))
+    if isinstance(args, list):
+        for i in range(0, len(args)):
+            position.append(i)
+            key_list = search_key(args[i], position, key_list)
+
+    elif isinstance(args, dict):
+        for key in args.keys():
+            if key in key_list:
+                key_list[key].append(position)
+            else:
+                key_list[key] = [position]
+
+            position.append(key)
+            key_list = search_key(args[key], position,key_list)
+            
+    else:
+        pass
+
+    return key_list
+
+
+    
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Parse JSON data')
@@ -27,15 +53,19 @@ if __name__ == "__main__":
 
     count = 0
 
-    if args.ss[count] in json_data.keys():
+    key_list = search_key(json_data, [], {})
+    
+    # if args.ss[count] in json_data.keys():
         
-        print(json_data[args.ss[count]])
-        print("type: %s" % type(json_data[args.ss[count]]))
-    else:
-        print("Not found keyname: %s" % args.ss[count])
-        quit()
+    #     print(json_data[args.ss[count]])
+    #     print("type: %s" % type(json_data[args.ss[count]]))
+    # else:
+    #     print("Not found keyname: %s" % args.ss[count])
+    #     quit()
     
 
+        
+        
            
 
     
